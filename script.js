0f8d733805ac84aec471f1f80f251790a6147cc9
thechildwithacap <84134728+thechildwithacap@users.noreply.github.com>
2023-12-19 18:43:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // script.js
 
+function sendRequest(endpoint) {
+    const apiUrl = 'http://your-raspberry-pi-ip:5000/' + endpoint;
+
+    fetch(apiUrl, { method: 'POST' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('status').innerText = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+
 // Function to send requests to control the water purifier
 function sendRequest(endpoint) {
     // You'll need to replace the placeholder URL with the actual endpoint of your Raspberry Pi
