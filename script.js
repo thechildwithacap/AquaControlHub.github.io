7ce60fb969ea0840b1cb8f7011a04b24ab00fb0e
thechildwithacap <84134728+thechildwithacap@users.noreply.github.com>
2023-12-19 18:26:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// script.js
+
+// Function to send requests to control the water purifier
+function sendRequest(endpoint) {
+    // You'll need to replace the placeholder URL with the actual endpoint of your Raspberry Pi
+    const apiUrl = 'http://your-raspberry-pi-ip/' + endpoint;
+
+    fetch(apiUrl, { method: 'POST' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            // Update the status div with the response from the server
+            document.getElementById('status').innerText = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+// Button click event handlers
+function activateWaterDispenser() {
+    sendRequest('activateWaterDispenser');
+}
+
+function turnOnOff() {
+    sendRequest('turnOnOff');
+}
+
+function activateHotWater() {
+    sendRequest('activateHotWater');
+}
+
+function activateNormalWater() {
+    sendRequest('activateNormalWater');
+}
+
+function activateColdWater() {
+    sendRequest('activateColdWater');
+}
